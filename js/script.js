1d2540387dda8f2afd4b64adef48b905be9d6be8
imonrana <info.imon@yahoo.com>
2024-10-05 02:49:03 +0600

    project password generator

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let inputField = document.getElementById("input_field");
+let btn = document.getElementById("btn");
+let displayPassword = document.getElementById("display_password");
+let charecter = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-={}[]|:;'<>,.?";
+
+// password genetartor js start
+
+const passwordGenerator = ()=>{
+    let password = "";
+    for(let i = 0; i<inputField.value; i++){
+        let result = Math.floor(Math.random() * charecter.length);
+        let randomLetter = charecter[result];
+        password += randomLetter;
+    }
+    displayPassword.innerHTML = password;
+}

9e3b5de29b0714b75b9c728f3a23969b04d2e317
imonrana <info.imon@yahoo.com>
2024-10-05 18:12:58 +0600

    Added css & some extra js features

--- js/script.js
+++ js/script.js
@@ -6,11 +6,16 @@ let charecter = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!
 // password genetartor js start
 
 const passwordGenerator = ()=>{
-    let password = "";
+    if(inputField.value == ""){
+        alert("please input number. how long password you want")
+    }
+    else{
+        let password = "";
     for(let i = 0; i<inputField.value; i++){
         let result = Math.floor(Math.random() * charecter.length);
         let randomLetter = charecter[result];
         password += randomLetter;
     }
     displayPassword.innerHTML = password;
+    }
 }
